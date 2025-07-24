30531531556d1a1ea6e8d93da091d55b441d5694
PigeonStrikesBack <dev.mauro.derosa@gmail.com>
2025-07-24 20:24:12 +0200

    Add mock task list display on GET / with links to individual task detail pages
    - Displays tasks with title and link
    - Uses route('tasks.show', ['id' => $task->id])
    - Returns "One single task" on /{id} route

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,23 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Main Page</title>
+    <title>Tasks</title>
 </head>
 <body>
-    <h1>Title</h1>
-    <h2>Subtitle</h2>
-    <p>Hello there, I'm a blade template!</p>
-
-    @isset($name)
-    <p>Your name is {{ $name }}</p>
-    @endisset
-
-    @isset($XSS)
-    <p>{{ $XSS }}</p>
-    @endisset
-
-    @isset($unsetVariable)
-    <marquee><span>This will never be rendered unless you set $unsetVariable: {{ $unsetVariable }}</span></marquee>
-    @endisset
+    <div>
+        <h1>Tasks</h1>
+        @forelse ($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', [
+            'id' => $task->id
+            ])}}">{{$task->title}}</a>
+        </div>
+        @empty
+        <h3>There's no tasks!</h3>
+        @endforelse
+    </div>
 </body>
 </html>
